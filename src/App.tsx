6d99721c1a815977c650abde1f862202bd0bee84
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-03-19 21:17:06 -0600

    refactor: reformat code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import {useState} from 'react'
 import './App.css'
 
-import {Button, Space} from 'antd'
+import { Button, Space } from 'antd'
+import { useState } from 'react'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div style={{padding: '0 24px'}}>
+      <div style={{ padding: '0 24px' }}>
         <Space>
-          <Button type="primary" onClick={() => setCount((count) => count + 1)}>+1</Button>
+          <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+            +1
+          </Button>
           <p>count is {count}</p>
         </Space>
       </div>
