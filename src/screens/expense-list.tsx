1e13de60d63ddd9686e1012a01a02f7b1ac53a27
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-03-20 21:07:56 -0600

    feat: display expenses in table

--- src/screens/expense-list.tsx
+++ src/screens/expense-list.tsx
@@ -1,3 +1,5 @@
+import { Table, TableProps } from 'antd'
+import Title from 'antd/es/typography/Title'
 import { useLoaderData } from 'react-router-dom'
 
 import { expenses } from '../data/demo-expenses.ts'
@@ -5,9 +7,30 @@ import { Expense } from '../model/expense.ts'
 
 export function ExpenseList() {
   const data = useLoaderData() as { expenses: Expense[] }
+
+  const columns: TableProps<Expense>['columns'] = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+      render: (_, { amount }) => <>EUR {amount.toFixed(2)}</>,
+    },
+  ]
+
   return (
     <>
-      <p>{JSON.stringify(data)}</p>
+      <Title level={2}>Expense List</Title>
+      <Table<Expense> columns={columns} dataSource={data.expenses} rowKey={'id'} pagination={false} />
     </>
   )
 }
