8bc4fb5fe32365f186743fdc62758147a4ad4000
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-03-21 07:31:26 -0600

    feat: use nested routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { ExpenseList, expenseListLoader } from './screens/expense-list.tsx'
+import { ExpenseRoot } from './screens/expense-root.tsx'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <ExpenseList />,
-    loader: expenseListLoader,
+    element: <ExpenseRoot />,
+    children: [
+      {
+        index: true,
+        element: <ExpenseList />,
+        loader: expenseListLoader,
+      },
+    ],
   },
 ])
