12ab1cea7bffb492c5ce11028c522ec005e07084
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-03-24 17:31:37 -0400

    feat: show expense-group overview as start page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,26 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { expenseGroupLoader } from './loader/expense-group-loader.ts'
+import { expenseGroupOverviewLoader } from './loader/expense-group-overview-loader.ts'
 import { AddExpense } from './screens/add-expense.tsx'
 import { EditExpense } from './screens/edit-expense.tsx'
+import { ExpenseGroupOverview } from './screens/expense-group-overview.tsx'
 import { ExpenseList } from './screens/expense-list.tsx'
 import { Root } from './screens/root.tsx'
 
 export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    loader: expenseGroupOverviewLoader,
+    id: 'expense-groups',
+    children: [
+      {
+        index: true,
+        element: <ExpenseGroupOverview />,
+      },
+    ],
+  },
   {
     path: '/:groupId',
     element: <Root />,
