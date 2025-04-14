ff73e17420503b455a7174b0c9f155aac0da61d3
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-04-14 19:42:13 +0200

    refactor: use two dashes to separate blocks from elements

--- src/components/expense-form.tsx
+++ src/components/expense-form.tsx
@@ -96,35 +96,35 @@ export function ExpenseForm({ members, expense, onSubmit }: ExpenseFormProps) {
     <>
       <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.set}>
-          <label htmlFor="title" className={titleValid ? styles.set_label : styles['set_label--invalid']}>
+          <label htmlFor="title" className={titleValid ? styles.set__label : styles['set_label--invalid']}>
             Title
           </label>
-          <input name="title" id="title" className={styles.set_input} onChange={handleTitleChange} value={title} />
+          <input name="title" id="title" className={styles.set__input} onChange={handleTitleChange} value={title} />
         </div>
         <div className={styles.set}>
-          <label htmlFor="amount" className={amountValid ? styles.set_label : styles['set_label--invalid']}>
+          <label htmlFor="amount" className={amountValid ? styles.set__label : styles['set_label--invalid']}>
             Amount
           </label>
-          <input name="amount" id="amount" className={styles.set_input} onChange={handleAmountChange} value={amount} />
+          <input name="amount" id="amount" className={styles.set__input} onChange={handleAmountChange} value={amount} />
         </div>
         <div className={styles.set}>
-          <label htmlFor="date" className={dateValid ? styles.set_label : styles['set_label--invalid']}>
+          <label htmlFor="date" className={dateValid ? styles.set__label : styles['set_label--invalid']}>
             Date
           </label>
           <input
             name="date"
             id="date"
             type="date"
-            className={styles.set_input}
+            className={styles.set__input}
             onChange={handleDateChanged}
             value={format(date, 'yyyy-MM-dd')}
           />
         </div>
         <div className={styles.set}>
-          <label htmlFor="paidBy" className={styles.set_label}>
+          <label htmlFor="paidBy" className={styles.set__label}>
             Paid by
           </label>
-          <select className={styles.set_input} onChange={handlePaidByChanged}>
+          <select className={styles.set__input} onChange={handlePaidByChanged}>
             {members.map((member) => (
               <option key={member} value={member} selected={member === paidBy}>
                 {member}
