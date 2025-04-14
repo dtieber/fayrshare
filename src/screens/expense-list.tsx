de1318f7484b0299142d7aa2a3c5c9006c464291
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-04-14 19:56:18 +0200

    feat: allow users to add new members to expense group
    the state coming from the router is not updated yet

--- src/screens/expense-list.tsx
+++ src/screens/expense-list.tsx
@@ -1,6 +1,7 @@
 import styles from './expense-list.module.css'
 
 import { format } from 'date-fns'
+import { useState } from 'react'
 import { NavLink, useRouteLoaderData } from 'react-router-dom'
 
 import { PageContent } from '../components/page-content.tsx'
@@ -9,6 +10,30 @@ import { ExpenseGroup } from '../model/expense-group.ts'
 
 export function ExpenseList() {
   const data = useRouteLoaderData('expenses') as ExpenseGroup
+  const [members, setMembers] = useState<string[]>(data.members)
+  const [emailEntered, setEmailEntered] = useState<string>('')
+  const [emailValid, setEmailValid] = useState(true)
+
+  function handleEmailKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    setEmailValid(true)
+
+    if (e.key !== 'Enter') {
+      return
+    }
+
+    if (!emailEntered.includes('@') || members.includes(emailEntered)) {
+      setEmailValid(false)
+      return
+    }
+
+    setMembers((prevMembers) => [...prevMembers, emailEntered])
+    setEmailEntered('')
+  }
+
+  function handleEmailEntered(e: React.ChangeEvent<HTMLInputElement>) {
+    setEmailEntered(e.currentTarget.value)
+  }
+
   return (
     <>
       <PageHeader title={data.name}>
@@ -24,6 +49,16 @@ export function ExpenseList() {
               {member}
             </span>
           ))}
+          <div className={styles.addMember}>
+            <input
+              name="addMember"
+              id="addMember"
+              onChange={handleEmailEntered}
+              onKeyDown={handleEmailKeyDown}
+              value={emailEntered}
+              className={emailValid ? styles.addMember__input : styles['addMember__input--invalid']}
+            />
+          </div>
         </div>
         <ul className={styles.list}>
           {data.expenses.map((expense) => (
